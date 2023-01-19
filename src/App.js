d0ac63c15beb1f316ccbc3bd613dd3204e168bc2
Paddy <Paddygnewton@hotmail.co.uk>
2023-01-19 13:08:35 +0000

    Updated stack, working on icons

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     flex-col
     items-center
     font-mono
+    overflow-auto
     "
     >
       <Header />
--- src/components/pages/Stack.jsx
+++ src/components/pages/Stack.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-
+// Commented out for testing
+// import {  DiPython, DiJava, DiJavascript, DiCss3, DiPhp, DiBootstrap, DiDotnet, DiHaskell , DiCode, DiDatabase, DiReact, } from 'react-icons/di';
 export default function Stack() {
   return (
     <>
       <div className="block bg-black text-green-500 p-4 m-6 rounded content-center w-3/5">
-        <h1 className="block text-xxxl text-green-500 mr-4 font-semibold text-center">
+        <h1 className="block text-xxxl text-green-500 mr-4 font-semibold text-center w-fit-content">
           My stack
         </h1>
         <p>
           I've been lucky enough to be exposed to a good variety of
-          technologies, languages and frameworks. However the language I tend to
-          resort to by default is most likely Java, though I am in the process
-          of turning that into Python. Through my career I hope to expand my
+          technologies, languages and frameworks. However the languages I tend to
+          resort to by default is Java and Python. Through my career I hope to expand my
           knowledge and experience of different tech as I'm always up for
-          learning something new
+          learning something new. Recently I have taken a large interest in ML, learning PyTorch and other supporting libraries.
         </p>
 
-        <div className="bg-black text-green-500 rounded text-center">
+        <div className="bg-black text-green-500 rounded text-center overflow-y-auto overflow-x-hidden">
           <div className="bg-gray-900 text-green-500 p-4 my-2 rounded">
             <h1 className="text-xxl font-bold">Frameworks and Tools</h1>
-            React - ASP.NET - Bootstrap - Tailwind - Bootstrap
+            {/* <h3 className="flex align-middle"><DiDotnet />ASP.NET - <DiBootstrap/>Bootstrap - <DiReact/>React - <DiCode/>Tailwind</h3> */}
+            ASP.Net - Bootstrap - React - Tailwind - PyTorch
           </div>
-          <div className="bg-gray-900 text-green-500 p-4 my-2 rounded">
+          <div className="bg-gray-900 text-green-500 p-4 my-2 rounded h-fit-content overflow-y-scroll overflow-x-hidden">
             <h1 className="text-xxl font-bold">Languages</h1>
-            Java - C - C# - Haskell - Javascript - HTML - CSS - SQL - PHP
+            {/* <h3 className="align-middle"><DiJava/>Java - <DiCode/>C# - <DiHaskell/>Haskell - <DiJavascript/>Javascript - <DiCode/>HTML - <DiCss3/>CSS - <DiDatabase/>SQL - <DiPhp/>PHP - <DiPython/>Python</h3> */}
+            Java - Haskell - C# - Javascript - HTML - CSS - SQL - PHP - Python
           </div>
           {/* <div className="bg-gray-900 text-green-500 p-4 my-2 rounded">
             <h1 className="text-xxl font-bold">Miscellaneous</h1>
