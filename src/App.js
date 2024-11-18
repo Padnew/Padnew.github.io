67bd9ce8ff3c386926ca31110175a938f2bf3175
Paddy <Paddygnewton@hotmail.co.uk>
2024-11-18 20:22:30 +0000

    Added scss files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
-import Stack from "./components/pages/stack";
+import TechStack from "./components/pages/stack";
 import Studies from "./components/pages/studies";
 import Home from "./components/pages/home";
-
+import Experience from "./components/pages/experience.jsx";
+import { Stack, Box } from "@mantine/core";
+import Classes from "./App.module.scss";
 function App() {
   return (
-    <div
-      className="h-screen w-screen
-    bg-gradient-to-b
-    from-black 
-    to-black
-    via-gray-900
-    font-mono
-    overflow-auto
-    text-sky-600
-    text-base
-    border-sky-500
-    "
-    >
+    <Box className={Classes.global}>
       <Header />
-      <div className="flex flex-col items-center">
+      <Stack justify="center" align="center">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
 
@@ -29,10 +19,12 @@ function App() {
 
           <Route exact path="/studies" element={<Studies />}></Route>
 
-          <Route exact path="/stack" element={<Stack />}></Route>
+          <Route exact path="/stack" element={<TechStack />}></Route>
+
+          <Route exact path="/experience" element={<Experience />}></Route>
         </Routes>
-      </div>
-    </div>
+      </Stack>
+    </Box>
   );
 }
 
