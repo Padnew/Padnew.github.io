c51f9e85825aeff136bfcc155ecbd990afdd0287
Paddy <Paddygnewton@hotmail.co.uk>
2024-11-23 15:55:26 +0000

    Header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,27 @@ function Header() {
       <Title fw={400} ml={5}>
         /paddy.software/
       </Title>
-      <Tabs defaultValue="home" color="red" variant="outline">
+      <Tabs defaultValue="home" variant="outline" color="red">
         <Tabs.List mx={0} px={0}>
           <Link to="/home">
-            <Tabs.Tab value="home">home</Tabs.Tab>
+            <Tabs.Tab value="home" fz={20}>
+              home
+            </Tabs.Tab>
           </Link>
           <Link to="/studies">
-            <Tabs.Tab value="studies">studies</Tabs.Tab>
+            <Tabs.Tab value="studies" fz={20}>
+              studies
+            </Tabs.Tab>
           </Link>
           <Link to="/stack">
-            <Tabs.Tab value="stack">my stack</Tabs.Tab>
+            <Tabs.Tab value="stack" fz={20}>
+              my stack
+            </Tabs.Tab>
           </Link>
           <Link to="/experience">
-            <Tabs.Tab value="experience">experience</Tabs.Tab>
+            <Tabs.Tab value="experience" fz={20}>
+              experience
+            </Tabs.Tab>
           </Link>
         </Tabs.List>
       </Tabs>
