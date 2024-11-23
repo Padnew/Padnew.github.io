665185c0200ff5d856ba1f35944663b27b491c7b
Paddy <Paddygnewton@hotmail.co.uk>
2024-11-23 16:29:33 +0000

    Add: experience page

---
+++ src/components/pages/experience.jsx
@@ -0,0 +1,10 @@
+import { Text } from "@mantine/core";
+import React from "react";
+
+export default function Experience() {
+  return (
+    <>
+      <Text color="white"> Under construction :^)</Text>
+    </>
+  );
+}
