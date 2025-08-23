25f1663ce7ea26794013d924f740cefc77c74bcd
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-23 11:08:39 +0100

    Refactor hero to use comitium

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,43 +1,43 @@
-import { Card, Text, useMantineTheme } from "@mantine/core";
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { Text, Card } from "comitium-ui";
 export const Hero = () => {
-  const theme = useMantineTheme();
   return (
     <>
       <Card
-        bg="black"
-        pos="relative"
-        display="block"
-        radius="md"
-        c={theme.colors.blue[4]}
-        p={4}
-        px={10}
-        fw={400}
-        fz={35}
-        ta="center"
+        curved="md"
         style={{
           border: "3px solid #9ecaed",
           outline: "none",
-          borderColor: "#9ecaed",
+          borderColor: "orange",
           boxShadow: "0 0 10px #9ecaed",
+          position: "relative",
+          display: "block",
+          padding: "10px 10px 4px 4px",
+          fontSize: 35,
+          fontWeight: 400,
+          borderRadius: "10px",
+          textAlign: "center",
         }}
-      >
-        <Typewriter
-          onInit={(typeWriter) => {
-            typeWriter
-              .typeString("Hey, my names Paddy")
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(500)
-              .typeString("Hey, my names Paddy")
-              .start();
-          }}
-        />
-        <Text fz="xl">
-          A full stack software engineer from Glasgow, Scotland.
-        </Text>
-      </Card>
+        footer={
+          <>
+            <Typewriter
+              onInit={(typeWriter) => {
+                typeWriter
+                  .typeString("Hey, my names Paddy")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .pauseFor(500)
+                  .typeString("Hey, my names Paddy")
+                  .start();
+              }}
+            />
+            <Text style={{ marginBottom: "10px" }}>
+              A full stack software engineer from Glasgow, Scotland.
+            </Text>
+          </>
+        }
+      />
     </>
   );
 };
