5f49740985029af5c555f4e85c5a029401fc00e1
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-23 11:12:40 +0100

    Removed unused classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import Content from "./components/Content.jsx";
-import Classes from "./App.module.scss";
 import { Container } from "comitium-ui";
 function App() {
   return (
-    <Container className={Classes.global}>
-      <Content className={Classes.global} />
+    <Container>
+      <Content />
     </Container>
   );
 }
