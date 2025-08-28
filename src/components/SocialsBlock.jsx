5d461b5b18c844e95f6c8d9529ddc5ccd49adc43
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-28 21:05:03 +0100

    Use comitium in socials block

--- src/components/SocialsBlock.jsx
+++ src/components/SocialsBlock.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import SocialsList from "./SocialsList";
-import { Card, Stack, Title, useMantineTheme } from "@mantine/core";
+import { Container, Flex, Text } from "comitium-ui";
 export const SocialsBlock = () => {
-  const theme = useMantineTheme();
   return (
-    <Card bg="inherit" align="center">
-      <Stack align="center" justify="center" gap={3}>
-        <Card bg="black" h="fit-content" py={5}>
-          <Title c={theme.colors.blue[4]} fw={400}>
-            Socials
-          </Title>
-        </Card>
+    <Container padding="sm">
+      <Flex
+        direction="column"
+        spacing="none"
+        style={{
+          textAlign: "center",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text size="xl">Socials</Text>
         <SocialsList />
-      </Stack>
-    </Card>
+      </Flex>
+    </Container>
   );
 };
