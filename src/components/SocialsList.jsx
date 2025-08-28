ca98a1da29a3f8c53169711ed705d43b42e4e9e1
Paddy <Paddygnewton@hotmail.co.uk>
2025-08-28 21:04:51 +0100

    Refactor mantine to comitium

--- src/components/SocialsList.jsx
+++ src/components/SocialsList.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
-import { Anchor, Group, useMantineTheme } from "@mantine/core";
+import { LinkTo, Flex } from "comitium-ui";
 import Socials from "../data/SocialsData";
 export default function SocialsList() {
-  const theme = useMantineTheme();
   return (
-    <Group p={10} gap={0}>
+    <Flex direction="row">
       {Socials.map((item) => (
         <motion.div
           key={item.id}
-          whileHover={{ scale: 1.1, borderColor: theme.colors.blue[5] }}
+          whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
           style={{
             overflow: "hidden",
@@ -18,19 +17,11 @@ export default function SocialsList() {
             padding: "10px",
           }}
         >
-          <Anchor
-            bg="black"
-            url={item.url}
-            m={5}
-            py={25}
-            px={10}
-            w="fit-content"
-            rel="_blank"
-          >
+          <LinkTo href={item.iconLink} target="_blank">
             <SocialIcon url={item.iconLink} />
-          </Anchor>
+          </LinkTo>
         </motion.div>
       ))}
-    </Group>
+    </Flex>
   );
 }
