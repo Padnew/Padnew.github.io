2eb422a72d32da55536e8ff2814da333ea5ae590
Paddy <Paddygnewton@hotmail.co.uk>
2025-09-02 17:47:53 +0100

    fix: utilise comitium on techstack

--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Flex, Text } from "comitium-ui";
+import { AutoGrid, Card, Text } from "comitium-ui";
 import { languagesArray, toolsArray } from "../../data/SiteData.tsx";
 import SkillAndIcon from "../shared/SkillAndIcon.tsx";
 
@@ -8,26 +8,30 @@ export default function TechStack() {
     <Card style={{ alignContent: "center" }}>
       <Text size={32}>Studies</Text>
       <Text>
-        Through both education, personal studies and professional experience I
-        have gained experience programming in a variety of languages, frameworks
-        and tools. I have thouroughly covered object orientated as well as
-        functional programming, styling, scripting as well as advanced source
-        control and database management
+        Through my higher education, personal studies as well as professional
+        experience I have gained experience programming in a variety of
+        languages, frameworks and tools. I have thouroughly covered object
+        orientated as well as functional programming, styling, scripting as well
+        as advanced source control and database management
       </Text>
-      <Flex direction="row">
-        <Card style={{ width: "50%" }}>
-          <Text size={24}>Tools</Text>
-          {toolsArray.map((tool) => {
-            return <SkillAndIcon key={tool.text} skill={tool} />;
-          })}
-        </Card>
-        <Card style={{ width: "50%" }}>
+      <AutoGrid>
+        <Card>
           <Text size={24}>Languages</Text>
-          {languagesArray.map((tool) => {
-            return <SkillAndIcon key={tool.text} skill={tool} />;
-          })}
+          <AutoGrid columns={4}>
+            {languagesArray.map((tool) => {
+              return <SkillAndIcon key={tool.text} skill={tool} />;
+            })}
+          </AutoGrid>
         </Card>
-      </Flex>
+        <Card>
+          <Text size={24}>Tools</Text>
+          <AutoGrid columns={4}>
+            {toolsArray.map((tool) => {
+              return <SkillAndIcon key={tool.text} skill={tool} />;
+            })}
+          </AutoGrid>
+        </Card>
+      </AutoGrid>
     </Card>
   );
 }
